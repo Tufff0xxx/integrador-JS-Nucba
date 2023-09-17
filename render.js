de5292d2d0c0c62863b9238118f9f9bba798c5cc
Maximiliano <tufox120@gmail.com>
2023-09-17 11:49:41 -0300

    integrador js1

--- render.js
+++ render.js
@@ -58,32 +58,46 @@ contenedor.addEventListener('click', (event) => {
     }
 });
 
-verCarrito.addEventListener('click', () =>{
+verCarrito.addEventListener('click', (event) =>{
+    event.preventDefault();
+    modalContenedor.innerHTML =''
     const modalHeader = document.createElement("div")
     modalHeader.className = 'modal-header'
     modalHeader.innerHTML = `
+    
     <h1 class="modal-header-titulo">Carrito</>
     `
     modalContenedor.append(modalHeader)
 
-    const modalButton = document.createElement('h1')
-    modalButton.innerText('X')
-    modalButton.className = 'modad-header-boton'
+   const modalButton = document.createElement('h1')
+    modalButton.innerText = 'X'
+    modalButton.className = 'modal-header-boton'
+    modalButton.addEventListener('click', () =>{
+        modalContenedor.style.display = 'none'
+    })
+    modalContenedor.append(modalButton)
 
-    carritoContent.forEach((product) => {
+    carrito.forEach((product) => {
         let carritoContent = document.createElement('div')
     carritoContent.className = 'modal-content'
     carritoContent.innerHTML = `
-    <img src="${product.cardImg}">
+    <img class="imagenCarrito" src="${product.cardImg}">
     <h3>${product.name}</h3>
     <p>${product.precio} $</p>
 
     `;
     modalContenedor.append(carritoContent)
 
-    })
+    });
+    
+    const total = carrito.reduce((acc,el) => acc + el.precio, 0)
+    
+    const totalBuying = document.createElement("div")
+    totalBuying.className = 'total-content'
+    totalBuying.innerHTML = `total a pagar: ${total} $`
+    modalContenedor.append(totalBuying)
 
-
+    modalContenedor.style.display = 'block';
 
     
 })
@@ -126,6 +140,49 @@ btnVerMas.addEventListener('click', () => {
 });
 
 mostrarProductos('Todos', productosMostrados);
+
+
+
+const init = () =>{
+    btnTodos.addEventListener('click', () => {
+        productosMostrados = 3;
+        categoriaActual = 'Todos';
+        mostrarProductos('Todos', productosMostrados);
+    });
+    
+    btnBatman.addEventListener('click', () => {
+        productosMostrados = 3;
+        categoriaActual = 'Batman';
+        mostrarProductos('Batman', productosMostrados);
+    });
+    
+    btnMarvel.addEventListener('click', () => {
+        productosMostrados = 3;
+        categoriaActual = 'Marvel';
+        mostrarProductos('Marvel', productosMostrados);
+    });
+    
+    btnSerie.addEventListener('click', () => {
+        productosMostrados = 3;
+        categoriaActual = 'Serie';
+        mostrarProductos('Serie', productosMostrados);
+    });
+    
+    btnDC.addEventListener('click', () => {
+        productosMostrados = 3;
+        categoriaActual = 'DC';
+        mostrarProductos('DC', productosMostrados);
+    });
+    
+    btnVerMas.addEventListener('click', () => {
+        productosMostrados += 3;
+        mostrarProductos(categoriaActual, productosMostrados);
+    });
+    
+    mostrarProductos('Todos', productosMostrados);
+}
+
+init ()
 
 
 
