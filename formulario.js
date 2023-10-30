a9bdd6783a84e7f882f12929ac6f775652049cb0
Maximiliano <tufox120@gmail.com>
2023-10-30 12:49:47 -0300

    PC COMPLETOvolver5

--- formulario.js
+++ formulario.js
@@ -12,27 +12,28 @@ document.addEventListener("DOMContentLoaded", function () {
         const email = document.getElementById("email").value;
 
         // Validar que los campos no estén vacíos
-        if (nombre === "" || apellido === "" || telefono === "" || email === "") {
+        if (nombre.trim() === "" || apellido.trim() === "" || telefono === "" || email === "") {
             mostrarMensajeError("Por favor, complete todos los campos.");
+        } else if (!/^[A-Za-z\s]+$/.test(nombre) || !/^[A-Za-z\s]+$/.test(apellido)) {
+            mostrarMensajeError("Nombre y apellido deben contener solo letras");
+        } else if (!/^\d+$/.test(telefono)) {
+            mostrarMensajeError("El campo de teléfono debe contener solo números.");
+        } else if (!/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(email)) {
+            mostrarMensajeError("El correo electrónico no es válido.");
         } else {
-            // Validar que el campo de teléfono contenga solo números
-            if (!esNumero(telefono)) {
-                mostrarMensajeError("El campo de teléfono debe contener solo números.");
-            } else {
-                // Guardar los datos en localStorage
-                const usuario = {
-                    nombre,
-                    apellido,
-                    telefono,
-                    email,
-                };
+            // Guardar los datos en localStorage
+            const usuario = {
+                nombre,
+                apellido,
+                telefono,
+                email,
+            };
 
-                localStorage.setItem("usuario", JSON.stringify(usuario));
-                mostrarMensajeExito("¡Registro exitoso!");
+            localStorage.setItem("usuario", JSON.stringify(usuario));
+            mostrarMensajeExito("¡Registro exitoso!");
 
-                // Limpiar el formulario
-                formulario.reset();
-            }
+            // Limpiar el formulario
+            formulario.reset();
         }
     });
 
